c600dd8fda5ee5225c7e7bf9bbc94639e7803e19
Clément Bellot <clement.bellot@ens-lyon.fr>
2023-12-01 11:17:23 +0100

    init

---
+++ 01/trebuchet.py
@@ -0,0 +1,20 @@
+import sys, re, os
+
+def partOne(inpu) :
+    with open(inpu,'r') as inp :
+    return
+
+print()
+print("==> PART ONE <==")
+print(partOne(sys.argv[1]))
+print()
+
+def partTwo(inpu) :
+    with open(inpu,'r') as inp :
+        pass
+    return 
+
+print("==> PART TWO <==")
+print(partTwo(sys.argv[1]))
+
+
